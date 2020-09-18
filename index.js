8c194fcfd5b9eae34fcbbd1ede0ebd993855b631
uafrazier <uafrazier@gmail.com>
2020-09-18 08:58:13 -0400

    updates path

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ var path = require("path");
 var app = express();
 var port = 8001;
 
-// app.get("/", function (req, res) {
-//   res.sendFile(__dirname + "/index.html");
-// });
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
 
-app.use(express.static(path.join(__dirname, "public")));
+//app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(port, "localhost", function () {
   console.log("Listening on port " + port);
