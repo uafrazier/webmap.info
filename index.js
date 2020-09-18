02b26122a1d49803c1e35ced17d68ef9b2b81f39
uafrazier <uafrazier@gmail.com>
2020-09-18 08:55:41 -0400

    updated path of resources

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var express = require("express");
+var path = require("path");
 var app = express();
 var port = 8001;
 
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/index.html/");
-});
+// app.get("/", function (req, res) {
+//   res.sendFile(__dirname + "/index.html");
+// });
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(port, "localhost", function () {
   console.log("Listening on port " + port);
