8daeb2e6a981701a1fad8d54bb3564d433311223
uafrazier <uafrazier@gmail.com>
2020-09-18 06:43:10 -0400

    updates index.html  path

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 var port = 8001;
 
-app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html/");
 });
 
-app.listen(port, 'localhost', function(){
-	console.log("Listening on port " + port);
+app.listen(port, "localhost", function () {
+  console.log("Listening on port " + port);
 });
